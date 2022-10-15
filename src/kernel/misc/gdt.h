e4374c7ccc356b9ceaf0f0bbbbbc9fc99907dbf0
Vasiliy Pereverzev <vasiliy.pereverzev@gmail.com>
2022-10-15 23:17:19 +0300

    fix

--- src/kernel/misc/gdt.h
+++ src/kernel/misc/gdt.h
@@ -36,123 +36,123 @@
 
 #ifdef __ASSEMBLER__
 
-#define SEG(flags_, base_, limit_)					\
-	.long ((limit_) & 0xffff) | (((base_) & 0xffff) << 16);		\
-	.long (((base_) >> 16) & 0xff) | (((flags_) & 0xf0ff) << 8) |	\
-		((limit_) & 0xf0000) | ((base_) & 0xff000000)
+#define SEG(flags_, base_, limit_)					                \
+    .long ((limit_) & 0xffff) | (((base_) & 0xffff) << 16);         \
+    .long (((base_) >> 16) & 0xff) | (((flags_) & 0xf0ff) << 8) |	\
+        ((limit_) & 0xf0000) | ((base_) & 0xff000000)
 
 #else
 
 #include <stdint.h>
 
 struct tss {
-	uint32_t reserved0;
-	uint64_t rsp0;
-	uint64_t rsp1;
-	uint64_t rsp2;
-	uint64_t reserved1;
-	uint64_t ist1;
-	uint64_t ist2;
-	uint64_t ist3;
-	uint64_t ist4;
-	uint64_t ist5;
-	uint64_t ist6;
-	uint64_t ist7;
-	uint64_t reserved2;
-	uint16_t reserved3;
-	uint16_t io_map_base;
+    uint32_t reserved0;
+    uint64_t rsp0;
+    uint64_t rsp1;
+    uint64_t rsp2;
+    uint64_t reserved1;
+    uint64_t ist1;
+    uint64_t ist2;
+    uint64_t ist3;
+    uint64_t ist4;
+    uint64_t ist5;
+    uint64_t ist6;
+    uint64_t ist7;
+    uint64_t reserved2;
+    uint16_t reserved3;
+    uint16_t io_map_base;
 } __attribute__((packed));
 
 struct descriptor {
-	union {
-		struct {
-			uint32_t a;
-			uint32_t b;
-		} dwords;
+    union {
+        struct {
+            uint32_t a;
+            uint32_t b;
+        } dwords;
 
-		struct {
-			uint16_t limit0;
-			uint16_t base0;
-			uint16_t base1: 8, type: 4, s: 1, dpl: 2, p: 1;
-			uint16_t limit: 4, avl: 1, l: 1, d: 1, g: 1, base2: 8;
-		} segment;
-	};
+        struct {
+            uint16_t limit0;
+            uint16_t base0;
+            uint16_t base1: 8, type: 4, s: 1, dpl: 2, p: 1;
+            uint16_t limit: 4, avl: 1, l: 1, d: 1, g: 1, base2: 8;
+        } segment;
+    };
 } __attribute__ ((packed));
 
 // See amd64 documentation (vol 2: System Programming)
 #define SEGMENT_DESC(flags_, base_, limit_) (struct descriptor){		\
-	.dwords = {								\
-		.a = ((limit_) & 0xffff) | (((base_) & 0xffff) << 16),		\
-		.b = (((base_) >> 16) & 0xff) | (((flags_) & 0xf0ff) << 8) |	\
-			((limit_) & 0xf0000) | ((base_) & 0xff000000),		\
-	}									\
+    .dwords = {								\
+        .a = ((limit_) & 0xffff) | (((base_) & 0xffff) << 16),		\
+        .b = (((base_) >> 16) & 0xff) | (((flags_) & 0xf0ff) << 8) |	\
+            ((limit_) & 0xf0000) | ((base_) & 0xff000000),		\
+    }									\
 }
 
 
 // XXX: 's' must be zero
 struct descriptor64 {
-	union {
-		struct {
-			uint32_t a;
-			uint32_t b;
-			uint32_t c;
-			uint32_t d;
-		} dwords;
+    union {
+        struct {
+            uint32_t a;
+            uint32_t b;
+            uint32_t c;
+            uint32_t d;
+        } dwords;
 
-		struct {
-			uint16_t target_offset0;
-			uint16_t target_selector;
-			uint16_t ist: 2, reserved0: 6, type: 4, s: 1, dpl: 2, p: 1;
-			uint16_t target_offset1;
-			uint32_t target_offset2;
-			uint32_t reserved1;
-		} interrupt, trap, gate;
+        struct {
+            uint16_t target_offset0;
+            uint16_t target_selector;
+            uint16_t ist: 2, reserved0: 6, type: 4, s: 1, dpl: 2, p: 1;
+            uint16_t target_offset1;
+            uint32_t target_offset2;
+            uint32_t reserved1;
+        } interrupt, trap, gate;
 
-		struct {
-			uint16_t segment_limit0;
-			uint16_t base_address0;
-			uint16_t base_address1: 8, type: 4, s: 1, dpl: 2, p: 1;
-			uint16_t segment_limit1: 4, avl: 1, ign: 2, g: 1, base_address2: 8;
-			uint32_t base_address3;
-			uint32_t mbz;
-		} tss;
-	};
+        struct {
+            uint16_t segment_limit0;
+            uint16_t base_address0;
+            uint16_t base_address1: 8, type: 4, s: 1, dpl: 2, p: 1;
+            uint16_t segment_limit1: 4, avl: 1, ign: 2, g: 1, base_address2: 8;
+            uint32_t base_address3;
+            uint32_t mbz;
+        } tss;
+    };
 } __attribute__ ((packed));
 
 #define GATE(sel_, off_, ist_, type_, dpl_) (struct descriptor64) {	\
-	.gate = {							\
-		.target_offset0 = ((off_) & 0xffff),			\
-		.target_selector = (sel_),				\
-		.ist = (ist_),						\
-		.type = (type_),					\
-		.s = 0,							\
-		.dpl = (dpl_),						\
-		.p = 1,							\
-		.target_offset1 = (((off_) >> 16) & 0xffff),		\
-		.target_offset2 = ((off_) >> 32),			\
-	}								\
+    .gate = {							\
+        .target_offset0 = ((off_) & 0xffff),			\
+        .target_selector = (sel_),				\
+        .ist = (ist_),						\
+        .type = (type_),					\
+        .s = 0,							\
+        .dpl = (dpl_),						\
+        .p = 1,							\
+        .target_offset1 = (((off_) >> 16) & 0xffff),		\
+        .target_offset2 = ((off_) >> 32),			\
+    }								\
 }
 
 #define TRAP_GATE(sel_, off_, ist_, dpl_) GATE(sel_, ((uint64_t)off_), ist_, TYPE_TRAP_GATE, dpl_)
 #define INTERRUPT_GATE(sel_, off_, ist_, dpl_) GATE(sel_, ((uint64_t)off_), ist_, TYPE_INTERRUPT_GATE, dpl_)
 
 #define TSS(base_, limit_, type_, dpl_) (struct descriptor64) {	\
-	.tss = {						\
-		.segment_limit0 = ((limit_) & 0xffff),		\
-		.base_address0 = ((base_) & 0xffff),		\
-		.base_address1 = (((base_) >> 16) & 0xff),	\
-		.type = (type_),				\
-		.s = 0,						\
-		.dpl = (dpl_),					\
-		.p = 1,						\
-		.segment_limit1 = (((limit_) >> 16) & 0xf),	\
-		.avl = 0,					\
-		.ign = 0,					\
-		.g = 0,						\
-		.base_address2 = (((base_) >> 24) & 0xff),	\
-		.base_address3 = ((base_ >> 32) & 0xffffffff),	\
-		.mbz = 0,					\
-	}							\
+    .tss = {						\
+        .segment_limit0 = ((limit_) & 0xffff),		\
+        .base_address0 = ((base_) & 0xffff),		\
+        .base_address1 = (((base_) >> 16) & 0xff),	\
+        .type = (type_),				\
+        .s = 0,						\
+        .dpl = (dpl_),					\
+        .p = 1,						\
+        .segment_limit1 = (((limit_) >> 16) & 0xf),	\
+        .avl = 0,					\
+        .ign = 0,					\
+        .g = 0,						\
+        .base_address2 = (((base_) >> 24) & 0xff),	\
+        .base_address3 = ((base_ >> 32) & 0xffffffff),	\
+        .mbz = 0,					\
+    }							\
 }
 
 #define SEGMENT_TSS(base, limit, type, dpl) TSS((uint64_t)base, (uint32_t)limit, type, dpl)
